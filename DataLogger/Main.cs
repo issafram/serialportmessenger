ef5e212c3fc4c6a1a1a9cba79d492f24b72a33b7
jjoyce4205 <jjoyce4205@c167584f-9348-0410-bb2f-71d5fc04abe4>
2008-03-26 19:38:15 +0000

    Started coding for DataLogger. Added function which opens Excel and creates a new workbook with 3 worksheets.

--- DataLogger/Main.cs
+++ DataLogger/Main.cs
@@ -5,14 +5,39 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
 
 namespace DataLogger
 {
-    public partial class Main : Form
+    public partial class Form1 : Form
     {
-        public Main()
+        public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Excel._Workbook objBook;
+            Excel.Workbooks objBooks;
+            Excel._Worksheet objSheet;
+            Excel.Sheets objSheets;
+            Excel.Application excelApp;
+            Excel.Range aRange;
+            excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            objBooks = excelApp.Workbooks;
+            objBook = objBooks.Add(Missing.Value);
+            objSheets = objBook.Worksheets;
+            objSheet = (Excel.Worksheet)objSheets.get_Item(1);
+            objSheet.Name = "Data Logger";
+            aRange = objSheet.get_Range("A1", Missing.Value);
+            aRange.Value2 = DateTime.Now.ToString();
+            aRange.ColumnWidth = 15;
+            //objBook.SaveAs(@"C:\Documents and Settings\Jon Joyce\Desktop\Test.xls", Excel.XlFileFormat.xlWorkbookNormal,
+            //                Missing.Value, Missing.Value, Missing.Value, Missing.Value, Excel.XlSaveAsAccessMode.xlNoChange,
+            //                Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
         }
     }
 }
