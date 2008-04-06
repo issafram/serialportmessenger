89f65b2810e35180705772124c0f6d9007eed1eb
Issa Fram <issafram@gmail.com>
2008-04-06 23:28:51 +0000

    Added the Flash window feature for when NEW messages arrive. Completely rewrote how data is SENT and RECEIVED.  Queue is now used to store received data (byte by byte).

--- SerialPortClient/Main.cs
+++ SerialPortClient/Main.cs
@@ -6,15 +6,26 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace SerialPortClient
 {
     public partial class Main : Form
     {
+        [DllImport("user32.dll")]
+        static extern bool FlashWindow(IntPtr hwnd, bool bInvert);
+
+
         private System.IO.Ports.SerialPort serial;
         private About about;
         private string userName;
         private string remoteUserName;
+
+        private byte[] b;
+        private Queue<byte> q = new Queue<byte>();
+        private bool fileMode = false;
+        private string data = "";
+        Thread dataProcessor;
 
         private DataLogger.Program dl;
         public Main()
@@ -31,8 +42,66 @@ namespace SerialPortClient
             remoteUserName = "";
         }
 
+        private void dataHandling()
+        {
+            while (q.Count > 0)
+            {
+                if (fileMode)
+                {
+                }
+                else
+                {
+                    data += ((char)(q.Dequeue())).ToString();
+                    if ((data.Length > 1) & (data.StartsWith("#")) & (data.EndsWith("#")))
+                    {
+                        string command = data.Substring(data.IndexOf("#") + 1, data.IndexOf("#", data.IndexOf("#") + 1) - 1);
+                        switch (command)
+                        {
+                            case "C":
+                                data = "";
+                                serial.Write("#CC#");
+                                statusLabel.Text = "Connected";
+                                serial.Write("#U#" + userName + "#EU#");
+                                break;
+                            case "CC":
+                                data = "";
+                                statusLabel.Text = "Connected";
+                                serial.Write("#U#" + userName + "#EU#");
+                                break;
+                            case "U":
+                                if ((data.Length > 7) & (data.EndsWith("#EU#")))
+                                {
+                                    remoteUserName = data.Substring(data.IndexOf("#U#") + 3, data.LastIndexOf("#EU#") - (data.IndexOf("#U#") + 3));
+                                    data = "";
+                                }
+                                break;
+                            case "E":
+                                statusLabel.Text = "Connection Closed - By Remote Client";
+                                Reset();
+                                break;
+                            case "M":
+                                if ((data.Length > 7) & (data.EndsWith("#EM#")))
+                                {
+                                    string message = data.Substring(data.IndexOf("#M#") + 3, data.LastIndexOf("#EM#") - (data.IndexOf("#M#") + 3));
+                                    data = "";
+                                    setText(txtHistory, txtHistory.Text + remoteUserName + " : " + message + System.Environment.NewLine);
+                                    dl.WriteLine(message);
+                                    message = null;
+                                }
+                                break;
+                        }
+                    }
+
+                }
+            }
+        }
+
         private void Main_Load(object sender, EventArgs e)
         {
+            //Control.CheckForIllegalCrossThreadCalls = false;
+            b = new byte[1];
+            //dataProcessor = new Thread(new ThreadStart(dataHandling));
+            //dataProcessor.Start();
             foreach (string s in System.IO.Ports.SerialPort.GetPortNames())
             {
                 cboPorts.Items.Add(s);
@@ -50,33 +119,22 @@ namespace SerialPortClient
 
         private void serial_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
         {
-            string data = serial.ReadLine();
-            string command = data.Substring(data.IndexOf("#") + 1, data.IndexOf("#", data.IndexOf("#") + 1) - 1);
-            switch (command)
+            while (serial.BytesToRead > 0)
             {
-                case "C":
-                    serial.WriteLine("#CC#");
-                    statusLabel.Text = "Connected";
-                    serial.WriteLine("#U#" + userName + "#EU#");
-                    break;
-                case "CC":
-                    statusLabel.Text = "Connected";
-                    serial.WriteLine("#U#" + userName + "#EU#");
-                    break;
-                case "U":
-                    remoteUserName = data.Substring(data.IndexOf("#U#") + 3, data.LastIndexOf("#EU#") - (data.IndexOf("#U#") + 3));
-                    break;
-                case "E":
-                    statusLabel.Text = "Connection Closed - By Remote Client";
-                    Reset();
-                    break;
-                case "M":
-                    string message = data.Substring(data.IndexOf("#M#") + 3, data.LastIndexOf("#EM#") - (data.IndexOf("#M#") + 3));
-                    setText(txtHistory, txtHistory.Text + remoteUserName + " : " +  message + System.Environment.NewLine);
-                    dl.WriteLine(message);
-                    message = null;
-                    break;
+                serial.Read(b, 0, 1);
+                q.Enqueue(b[0]);
             }
+            //Thread.Sleep(1000);
+            //MessageBox.Show("RECEIVED DATA");
+            dataHandling();
+            //if (dataProcessor.ThreadState != ThreadState.Running)
+            //{
+            //    dataProcessor = new Thread(new ThreadStart(dataHandling));
+            //    dataProcessor.Priority = ThreadPriority.Lowest;
+            //    dataProcessor.Start();
+            //}
+            
+            
         }
 
         private void Reset()
@@ -98,6 +156,9 @@ namespace SerialPortClient
             else
             {
                 textBox.Text = text;
+                textBox.SelectionStart = textBox.Text.Length - 1;
+                textBox.ScrollToCaret();
+                FlashWindow(this.Handle, true);
             }
             
         }
@@ -108,7 +169,7 @@ namespace SerialPortClient
             serial.Open();
             serial.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(serial_DataReceived);
             statusLabel.Text = "Connecting...";
-            serial.WriteLine("#C#");
+            serial.Write("#C#");
         }
 
         private void mnuExit_Click(object sender, EventArgs e)
@@ -123,10 +184,18 @@ namespace SerialPortClient
             {
                 if (serial.IsOpen)
                 {
-                    serial.WriteLine("#E#");
+                    serial.Write("#E#");
                     serial.Close();
                 }
             }
+            //if (dataProcessor.ThreadState == ThreadState.Running)
+            //{
+                //while (dataProcessor.ThreadState != ThreadState.Suspended)
+                //{
+                    //dataProcessor.Suspend();
+                    ////dataProcessor.Abort();
+                //}
+            //}
         }
 
         private void cboPorts_SelectedIndexChanged(object sender, EventArgs e)
@@ -147,7 +216,7 @@ namespace SerialPortClient
         {
             if (txtMessage.Text != "")
             {
-                serial.WriteLine("#M#" + txtMessage.Text + "#EM#");
+                serial.Write("#M#" + txtMessage.Text + "#EM#");
                 setText(txtHistory, txtHistory.Text + userName + " : " + txtMessage.Text + System.Environment.NewLine);
                 txtMessage.Text = "";
                 txtMessage.Focus();
