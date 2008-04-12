edb4b74cc85413bb3e49a1be4783fef448a117f8
jjoyce4205 <jjoyce4205@c167584f-9348-0410-bb2f-71d5fc04abe4>
2008-04-12 23:48:31 +0000

    Added Create User Button.  Check existence of a user that already exists.

--- SerialPortClient/Login.Designer.cs
+++ SerialPortClient/Login.Designer.cs
@@ -35,15 +35,17 @@ namespace SerialPortClient
             this.lblPassword = new System.Windows.Forms.Label();
             this.grpLogin = new System.Windows.Forms.GroupBox();
             this.btnMicro = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
             this.grpLogin.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnLogin
             // 
             this.btnLogin.Enabled = false;
-            this.btnLogin.Location = new System.Drawing.Point(124, 96);
+            this.btnLogin.Location = new System.Drawing.Point(57, 75);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.Size = new System.Drawing.Size(56, 19);
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
@@ -51,71 +53,91 @@ namespace SerialPortClient
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(99, 28);
+            this.txtUsername.Location = new System.Drawing.Point(74, 23);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(100, 22);
+            this.txtUsername.Size = new System.Drawing.Size(76, 20);
             this.txtUsername.TabIndex = 0;
             this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(99, 56);
+            this.txtPassword.Location = new System.Drawing.Point(74, 46);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtPassword.Size = new System.Drawing.Size(76, 20);
             this.txtPassword.TabIndex = 1;
-            this.txtPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPassword_KeyPress);
             this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            this.txtPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPassword_KeyPress);
             // 
             // lblUsername
             // 
             this.lblUsername.AutoSize = true;
-            this.lblUsername.Location = new System.Drawing.Point(20, 28);
+            this.lblUsername.Location = new System.Drawing.Point(15, 23);
+            this.lblUsername.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(73, 17);
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
             this.lblUsername.TabIndex = 3;
             this.lblUsername.Text = "Username";
             // 
             // lblPassword
             // 
             this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(24, 59);
+            this.lblPassword.Location = new System.Drawing.Point(18, 48);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(69, 17);
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
             this.lblPassword.TabIndex = 4;
             this.lblPassword.Text = "Password";
             // 
             // grpLogin
             // 
+            this.grpLogin.Controls.Add(this.btnCreate);
             this.grpLogin.Controls.Add(this.txtPassword);
             this.grpLogin.Controls.Add(this.lblPassword);
             this.grpLogin.Controls.Add(this.btnLogin);
             this.grpLogin.Controls.Add(this.lblUsername);
             this.grpLogin.Controls.Add(this.txtUsername);
-            this.grpLogin.Location = new System.Drawing.Point(12, 12);
+            this.grpLogin.Location = new System.Drawing.Point(9, 10);
+            this.grpLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.grpLogin.Name = "grpLogin";
-            this.grpLogin.Size = new System.Drawing.Size(223, 153);
+            this.grpLogin.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grpLogin.Size = new System.Drawing.Size(167, 124);
             this.grpLogin.TabIndex = 5;
             this.grpLogin.TabStop = false;
             this.grpLogin.Text = "Login";
             // 
             // btnMicro
             // 
-            this.btnMicro.Location = new System.Drawing.Point(12, 186);
+            this.btnMicro.Location = new System.Drawing.Point(9, 151);
+            this.btnMicro.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnMicro.Name = "btnMicro";
-            this.btnMicro.Size = new System.Drawing.Size(223, 23);
+            this.btnMicro.Size = new System.Drawing.Size(167, 19);
             this.btnMicro.TabIndex = 3;
             this.btnMicro.Text = "Connect to Microcontroller";
             this.btnMicro.UseVisualStyleBackColor = true;
             this.btnMicro.Click += new System.EventHandler(this.btnMicro_Click);
             // 
+            // btnCreate
+            // 
+            this.btnCreate.Enabled = false;
+            this.btnCreate.Location = new System.Drawing.Point(18, 99);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(132, 20);
+            this.btnCreate.TabIndex = 7;
+            this.btnCreate.Text = "Create New Profile";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(247, 228);
+            this.ClientSize = new System.Drawing.Size(185, 185);
             this.Controls.Add(this.btnMicro);
             this.Controls.Add(this.grpLogin);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Login";
             this.Text = "SPM - Login";
             this.Load += new System.EventHandler(this.Login_Load);
@@ -134,5 +156,6 @@ namespace SerialPortClient
         private System.Windows.Forms.Label lblPassword;
         private System.Windows.Forms.GroupBox grpLogin;
         private System.Windows.Forms.Button btnMicro;
+        private System.Windows.Forms.Button btnCreate;
     }
 }
