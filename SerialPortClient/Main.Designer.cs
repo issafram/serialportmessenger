a114192ee41282fc0088589b31128bee39e45cba
Issa Fram <issafram@gmail.com>
2008-04-15 03:29:08 +0000

    Changed TextBox to RichTextBox for history and message send boxes. Now allows for adding and sending emoticons. To save time and minimize data being sent, I have the program only send an image code of the emoticon which then gets replaced with an image on the receiving end. Added hidden richtextbox to parse RTF data. Added ComboBoxEx class which allows for images in combo boxes.

--- SerialPortClient/Main.Designer.cs
+++ SerialPortClient/Main.Designer.cs
@@ -28,10 +28,11 @@ namespace SerialPortClient
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.btnListen = new System.Windows.Forms.Button();
             this.btnConnect = new System.Windows.Forms.Button();
             this.cboPorts = new System.Windows.Forms.ComboBox();
-            this.txtMessage = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fIleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -44,7 +45,11 @@ namespace SerialPortClient
             this.mnuAbout = new System.Windows.Forms.ToolStripMenuItem();
             this.status = new System.Windows.Forms.StatusStrip();
             this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
-            this.txtHistory = new System.Windows.Forms.TextBox();
+            this.txtHistory = new System.Windows.Forms.RichTextBox();
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.imgIcons = new System.Windows.Forms.ImageList(this.components);
+            this.txtTemp = new System.Windows.Forms.RichTextBox();
+            this.imageCombo = new SerialPortClient.ComboBoxEx();
             this.menuStrip1.SuspendLayout();
             this.status.SuspendLayout();
             this.SuspendLayout();
@@ -52,10 +57,10 @@ namespace SerialPortClient
             // btnListen
             // 
             this.btnListen.Enabled = false;
-            this.btnListen.Location = new System.Drawing.Point(192, 52);
-            this.btnListen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnListen.Location = new System.Drawing.Point(256, 64);
+            this.btnListen.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnListen.Name = "btnListen";
-            this.btnListen.Size = new System.Drawing.Size(56, 19);
+            this.btnListen.Size = new System.Drawing.Size(75, 23);
             this.btnListen.TabIndex = 0;
             this.btnListen.Text = "Listen";
             this.btnListen.UseVisualStyleBackColor = true;
@@ -64,10 +69,10 @@ namespace SerialPortClient
             // btnConnect
             // 
             this.btnConnect.Enabled = false;
-            this.btnConnect.Location = new System.Drawing.Point(24, 52);
-            this.btnConnect.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnConnect.Location = new System.Drawing.Point(32, 64);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(56, 19);
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
             this.btnConnect.TabIndex = 1;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -76,34 +81,20 @@ namespace SerialPortClient
             // cboPorts
             // 
             this.cboPorts.FormattingEnabled = true;
-            this.cboPorts.Location = new System.Drawing.Point(94, 25);
-            this.cboPorts.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboPorts.Location = new System.Drawing.Point(125, 31);
+            this.cboPorts.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.cboPorts.Name = "cboPorts";
-            this.cboPorts.Size = new System.Drawing.Size(92, 21);
+            this.cboPorts.Size = new System.Drawing.Size(121, 24);
             this.cboPorts.TabIndex = 2;
             this.cboPorts.Text = "Select Port";
             this.cboPorts.SelectedIndexChanged += new System.EventHandler(this.cboPorts_SelectedIndexChanged);
             // 
-            // txtMessage
-            // 
-            this.txtMessage.Location = new System.Drawing.Point(16, 256);
-            this.txtMessage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtMessage.Multiline = true;
-            this.txtMessage.Name = "txtMessage";
-            this.txtMessage.Size = new System.Drawing.Size(234, 76);
-            this.txtMessage.TabIndex = 3;
-            this.txtMessage.Text = "Enter message";
-            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
-            this.txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyDown);
-            this.txtMessage.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyUp);
-            this.txtMessage.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMessage_KeyPress);
-            // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(192, 341);
-            this.btnSend.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnSend.Location = new System.Drawing.Point(256, 420);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(56, 19);
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
             this.btnSend.TabIndex = 4;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -117,8 +108,8 @@ namespace SerialPortClient
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(280, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(373, 28);
             this.menuStrip1.TabIndex = 5;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -128,20 +119,20 @@ namespace SerialPortClient
             this.sendFileToolStripMenuItem,
             this.mnuExit});
             this.fIleToolStripMenuItem.Name = "fIleToolStripMenuItem";
-            this.fIleToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fIleToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
             this.fIleToolStripMenuItem.Text = "File";
             // 
             // sendFileToolStripMenuItem
             // 
             this.sendFileToolStripMenuItem.Name = "sendFileToolStripMenuItem";
-            this.sendFileToolStripMenuItem.Size = new System.Drawing.Size(128, 22);
+            this.sendFileToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
             this.sendFileToolStripMenuItem.Text = "Send File";
             this.sendFileToolStripMenuItem.Click += new System.EventHandler(this.sendFileToolStripMenuItem_Click);
             // 
             // mnuExit
             // 
             this.mnuExit.Name = "mnuExit";
-            this.mnuExit.Size = new System.Drawing.Size(128, 22);
+            this.mnuExit.Size = new System.Drawing.Size(138, 24);
             this.mnuExit.Text = "Exit";
             this.mnuExit.Click += new System.EventHandler(this.mnuExit_Click);
             // 
@@ -151,19 +142,19 @@ namespace SerialPortClient
             this.baudRateToolStripMenuItem1,
             this.dataLoggerToolStripMenuItem});
             this.baudRateToolStripMenuItem.Name = "baudRateToolStripMenuItem";
-            this.baudRateToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.baudRateToolStripMenuItem.Size = new System.Drawing.Size(73, 24);
             this.baudRateToolStripMenuItem.Text = "Options";
             // 
             // baudRateToolStripMenuItem1
             // 
             this.baudRateToolStripMenuItem1.Name = "baudRateToolStripMenuItem1";
-            this.baudRateToolStripMenuItem1.Size = new System.Drawing.Size(144, 22);
+            this.baudRateToolStripMenuItem1.Size = new System.Drawing.Size(161, 24);
             this.baudRateToolStripMenuItem1.Text = "Baud Rate";
             // 
             // dataLoggerToolStripMenuItem
             // 
             this.dataLoggerToolStripMenuItem.Name = "dataLoggerToolStripMenuItem";
-            this.dataLoggerToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.dataLoggerToolStripMenuItem.Size = new System.Drawing.Size(161, 24);
             this.dataLoggerToolStripMenuItem.Text = "Data Logger";
             this.dataLoggerToolStripMenuItem.Click += new System.EventHandler(this.dataLoggerToolStripMenuItem_Click);
             // 
@@ -172,13 +163,13 @@ namespace SerialPortClient
             this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.mnuAbout});
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
             this.helpToolStripMenuItem.Text = "Help";
             // 
             // mnuAbout
             // 
             this.mnuAbout.Name = "mnuAbout";
-            this.mnuAbout.Size = new System.Drawing.Size(152, 22);
+            this.mnuAbout.Size = new System.Drawing.Size(143, 24);
             this.mnuAbout.Text = "SPM Help";
             this.mnuAbout.Click += new System.EventHandler(this.mnuAbout_Click);
             // 
@@ -186,48 +177,95 @@ namespace SerialPortClient
             // 
             this.status.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.statusLabel});
-            this.status.Location = new System.Drawing.Point(0, 372);
+            this.status.Location = new System.Drawing.Point(0, 460);
             this.status.Name = "status";
-            this.status.Padding = new System.Windows.Forms.Padding(1, 0, 10, 0);
-            this.status.Size = new System.Drawing.Size(280, 22);
+            this.status.Padding = new System.Windows.Forms.Padding(1, 0, 13, 0);
+            this.status.Size = new System.Drawing.Size(373, 25);
             this.status.TabIndex = 8;
             // 
             // statusLabel
             // 
             this.statusLabel.Name = "statusLabel";
-            this.statusLabel.Size = new System.Drawing.Size(25, 17);
+            this.statusLabel.Size = new System.Drawing.Size(34, 20);
             this.statusLabel.Text = "Idle";
             // 
             // txtHistory
             // 
-            this.txtHistory.Location = new System.Drawing.Point(16, 83);
-            this.txtHistory.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.txtHistory.Multiline = true;
+            this.txtHistory.Location = new System.Drawing.Point(21, 102);
             this.txtHistory.Name = "txtHistory";
             this.txtHistory.ReadOnly = true;
-            this.txtHistory.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txtHistory.Size = new System.Drawing.Size(234, 161);
-            this.txtHistory.TabIndex = 9;
+            this.txtHistory.Size = new System.Drawing.Size(310, 197);
+            this.txtHistory.TabIndex = 10;
+            this.txtHistory.Text = "";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(21, 330);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(310, 89);
+            this.txtMessage.TabIndex = 11;
+            this.txtMessage.Text = "";
+            this.txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyDown);
+            this.txtMessage.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMessage_KeyPress);
+            this.txtMessage.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyUp);
+            // 
+            // imgIcons
+            // 
+            this.imgIcons.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgIcons.ImageStream")));
+            this.imgIcons.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgIcons.Images.SetKeyName(0, "icon_biggrin.gif");
+            this.imgIcons.Images.SetKeyName(1, "icon_confused.gif");
+            this.imgIcons.Images.SetKeyName(2, "icon_cool.gif");
+            this.imgIcons.Images.SetKeyName(3, "icon_eek.gif");
+            this.imgIcons.Images.SetKeyName(4, "icon_mad.gif");
+            this.imgIcons.Images.SetKeyName(5, "icon_sad.gif");
+            this.imgIcons.Images.SetKeyName(6, "icon_smile.gif");
+            this.imgIcons.Images.SetKeyName(7, "icon_surprised.gif");
+            // 
+            // txtTemp
+            // 
+            this.txtTemp.Location = new System.Drawing.Point(256, 31);
+            this.txtTemp.Name = "txtTemp";
+            this.txtTemp.Size = new System.Drawing.Size(56, 28);
+            this.txtTemp.TabIndex = 14;
+            this.txtTemp.Text = "";
+            this.txtTemp.Visible = false;
+            // 
+            // imageCombo
+            // 
+            this.imageCombo.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.imageCombo.DropDownHeight = 200;
+            this.imageCombo.FormattingEnabled = true;
+            this.imageCombo.ImageList = this.imgIcons;
+            this.imageCombo.IntegralHeight = false;
+            this.imageCombo.Location = new System.Drawing.Point(210, 305);
+            this.imageCombo.Name = "imageCombo";
+            this.imageCombo.Size = new System.Drawing.Size(121, 23);
+            this.imageCombo.TabIndex = 13;
+            this.imageCombo.Text = "Emoticons";
+            this.imageCombo.SelectedIndexChanged += new System.EventHandler(this.imageCombo_SelectedIndexChanged);
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(280, 394);
-            this.Controls.Add(this.txtHistory);
+            this.ClientSize = new System.Drawing.Size(373, 485);
+            this.Controls.Add(this.txtTemp);
+            this.Controls.Add(this.imageCombo);
             this.Controls.Add(this.status);
             this.Controls.Add(this.btnSend);
-            this.Controls.Add(this.txtMessage);
             this.Controls.Add(this.cboPorts);
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.btnListen);
             this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.txtHistory);
+            this.Controls.Add(this.txtMessage);
             this.MainMenuStrip = this.menuStrip1;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Main";
             this.Text = "Serial Port Data Transfer";
-            this.Load += new System.EventHandler(this.Main_Load);
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Main_FormClosed);
+            this.Load += new System.EventHandler(this.Main_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.status.ResumeLayout(false);
@@ -242,7 +280,6 @@ namespace SerialPortClient
         private System.Windows.Forms.Button btnListen;
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.ComboBox cboPorts;
-        private System.Windows.Forms.TextBox txtMessage;
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fIleToolStripMenuItem;
@@ -254,8 +291,12 @@ namespace SerialPortClient
         private System.Windows.Forms.ToolStripMenuItem mnuAbout;
         private System.Windows.Forms.StatusStrip status;
         private System.Windows.Forms.ToolStripStatusLabel statusLabel;
-        private System.Windows.Forms.TextBox txtHistory;
         private System.Windows.Forms.ToolStripMenuItem sendFileToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox txtHistory;
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private ComboBoxEx imageCombo;
+        private System.Windows.Forms.ImageList imgIcons;
+        private System.Windows.Forms.RichTextBox txtTemp;
     }
 }
 
